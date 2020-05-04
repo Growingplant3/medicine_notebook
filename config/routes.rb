5d3ed710b083d57b2867902cad75e93c93a081e6
下田　佳紀 <y.s@experience.local>
2020-05-04 17:11:18 +0900

    ナビバーの整備1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  #devise_for :users, controllers: { registrations: 'users/registrations' }
-  get "users/index" => "users#index"
+  devise_for :users, :controllers => {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+  end
+  get "users/show" => "users#show"
   root 'users#index'
 end
