1d7e72c4c6e646f81d7fece8f038a3a41b193164
下田　佳紀 <y.s@experience.local>
2020-05-16 21:22:13 +0900

    体調グラフ2

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,6 +1,13 @@
 class ConditionsController < ApplicationController
   def index
     @conditions = current_user.conditions
+    @chart_map = {"systolic_blood_pressure" => [], "diastolic_blood_pressure" => [], "pulse" => [], "blood_sugar" => []}
+    @conditions.each do |condition|
+      @chart_map["systolic_blood_pressure"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.systolic_blood_pressure])
+      @chart_map["diastolic_blood_pressure"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.diastolic_blood_pressure])
+      @chart_map["pulse"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.pulse])
+      @chart_map["blood_sugar"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.blood_sugar])
+    end
   end
 
   def edit
