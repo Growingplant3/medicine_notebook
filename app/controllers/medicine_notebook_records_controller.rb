df57b6c96ddaaf2667912b5bae439dd8520ff948
下田　佳紀 <y.s@experience.local>
2020-05-23 20:36:38 +0900

    入力フォーム作成中2

--- app/controllers/medicine_notebook_records_controller.rb
+++ app/controllers/medicine_notebook_records_controller.rb
@@ -1,8 +1,8 @@
 class MedicineNotebookRecordsController < ApplicationController
   before_action :authenticate_pharmacy!, only: [:search]
   def new
-    @user = User.find(params[:id])
-    @medicine_notebook_record = MedicineNotebookRecord.new(user_id: @user.id,pharmacy_id: current_pharmacy.id)
+    one_user = @user = User.find(params[:id])
+    @medicine_notebook_record = MedicineNotebookRecord.new
     @drug_information = @medicine_notebook_record.drug_informations.build
     @drug_information.how_to_takes.build
     redirect_to root_path unless pharmacy_signed_in?
@@ -11,10 +11,13 @@ class MedicineNotebookRecordsController < ApplicationController
 
   def edit
   end
-  
+
   def create
-    @medicine_notebook_record.save(params)
-    show_path
+    a = MedicineNotebookRecord.new(medicine_notebook_record_params)
+    a.user_id = params[:id]
+    a.pharmacy_id = current_pharmacy.id
+    a.save
+    redirect_to show_path
   end
 
   def show
@@ -26,5 +29,12 @@ class MedicineNotebookRecordsController < ApplicationController
   end
 
   def select
+  end
+
+  private
+  def medicine_notebook_record_params
+    params.require(:medicine_notebook_record).permit(:date_of_issue, :date_of_dispensing, :medical_institution, :doctor_name, :attached_comment,
+     drug_informations_attributes: [:user_id, :pharmacy_id, :drug_name, :daily_dose, :prescription_days, :medical_effect, :crush, :shading, :one_dose_package, :attention, :_destroy,
+      how_to_takes_attributes: [:number_of_doses, :when_to_take, :_destroy]])
   end
 end
