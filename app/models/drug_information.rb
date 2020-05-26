b538018b29d41c4630bdb7a879ba22f21433833d
下田　佳紀 <y.s@experience.local>
2020-05-27 03:40:07 +0900

    viewの修正5

--- app/models/drug_information.rb
+++ app/models/drug_information.rb
@@ -2,9 +2,35 @@ class DrugInformation < ApplicationRecord
   enum drug_name: { :amlodipine_2_5 => 0,
   :amlodipine_5 => 1,
   :rosuvastatin_2_5 => 2,
-  :rosuvastatin_5 => 3,  
-  }
-  enum medical_effect: { :antihypertensive => 0, :lipid_abnormalities => 1}
+  :rosuvastatin_5 => 3,
+  :xarelto_10 => 4,
+  :xarelto_15 => 5,
+  :eliquis_2_5 => 6,
+  :eliquis_5 => 7,
+  :e_keppra_250 => 8,
+  :e_keppra_500 => 9,
+  :artist_1_2_5 => 10,
+  :artist_2_5 => 11,
+  :artist_10 => 12,
+  :artist_20 => 13,
+  :olmetec_5 => 14,
+  :olmetec_10 => 15,
+  :olmetec_20 => 16,
+  :olmetec_40 => 17,
+  :ancaron_100 => 18,
+  :lasix_10 => 19,
+  :lasix_20 => 20,
+  :lasix_40 => 21,
+  :clarith_50 => 22,
+  :kipres_chewable_5 => 23}
+  enum medical_effect: { :anti_hypertensive => 0,
+  :lipid_abnormalities => 1,
+  :blood_flow_smoothly => 2,
+  :arrhythmia => 3,
+  :anti_epileptic_drug => 4,
+  :heart_disease => 5,
+  :anti_bacterial_drug => 6,
+  :anti_allergic_drug => 7}
   enum crush: { :as_it_is => false, :smash => true }
   enum shading: { :light => false, :dark => true }
   enum one_dose_package: { :tablet => false, :pouch => true }
