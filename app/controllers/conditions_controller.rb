c04c29962cfa57753b7d4cbad999178878284da8
下田　佳紀 <y.s@experience.local>
2020-06-01 23:33:14 +0900

    細かな修正

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,12 +1,12 @@
 class ConditionsController < ApplicationController
   def index
     @conditions = current_user.conditions
-    @chart_map = {"systolic_blood_pressure" => [], "diastolic_blood_pressure" => [], "pulse" => [], "blood_sugar" => []}
+    @chart_map = {"収縮期血圧" => [], "拡張期血圧" => [], "脈拍" => [], "血糖値" => []}
     @conditions.each do |condition|
-      @chart_map["systolic_blood_pressure"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.systolic_blood_pressure])
-      @chart_map["diastolic_blood_pressure"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.diastolic_blood_pressure])
-      @chart_map["pulse"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.pulse])
-      @chart_map["blood_sugar"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.blood_sugar])
+      @chart_map["収縮期血圧"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.systolic_blood_pressure])
+      @chart_map["拡張期血圧"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.diastolic_blood_pressure])
+      @chart_map["脈拍"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.pulse])
+      @chart_map["血糖値"].push([condition.updated_at.strftime('%Y/%m/%d %H:%M:%S'), condition.blood_sugar])
     end
   end
 
